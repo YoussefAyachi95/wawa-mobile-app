15c9494cddc5d1af9583e4688ea91f08dbbdf027
Youssef <youssef1995elam@gmail.com>
2024-02-08 14:14:10 +0100

    Feature: Autocomplete functionality

--- app/(modal)/LocationSearch.tsx
+++ app/(modal)/LocationSearch.tsx
@@ -1,9 +1,16 @@
 import { View, Text, StyleSheet } from 'react-native'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import MapView from 'react-native-maps'
 import { TouchableOpacity } from 'react-native-gesture-handler'
 import Colors from '@/constants/Colors'
 import { useNavigation } from 'expo-router'
+import Autocomplete from 'react-native-autocomplete-input';
+import axios from 'axios';
+
+interface Prediction {
+    description: string;
+}
+
 
 const LocationSearch = () => {
     const navigation = useNavigation()
@@ -13,10 +20,71 @@ const LocationSearch = () => {
         latitudeDelta: 0.02,
         longitudeDelta: 0.02,
     })
+    const [query, setQuery] = useState<string>('');
+    const [suggestions, setSuggestions] = useState<string[]>([]);
+    const [isTyping, setIsTyping] = useState<boolean>(false);
 
+    useEffect(() => {
+        const debounce = setTimeout(() => {
+            if (isTyping && query.trim() !== '') {
+                fetchSuggestions();
+            }
+        }, 500);
+
+        return () => clearTimeout(debounce);
+    }, [query, isTyping]);
+
+    const fetchSuggestions = async () => {
+        try {
+            const response = await axios.get<{ predictions: Prediction[] }>(process.env.EXPO_PUBLIC_RAPID_API_URL as string, {
+                params: {
+                    input: query,
+                    radius: '50000'
+                },
+                headers: {
+                    'X-RapidAPI-Key': process.env.EXPO_PUBLIC_RAPID_API_KEY,
+                    'X-RapidAPI-Host': process.env.EXPO_PUBLIC_RAPID_API_HOST
+                }
+            });
+            if (response.data && response.data.predictions) {
+                setSuggestions(response.data.predictions.map(prediction => prediction.description));
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const handleInputChange = (text: string) => {
+        setIsTyping(true);
+        setQuery(text);
+        if (text.trim() === '') {
+            setSuggestions([]);
+        }
+    };
+
+    const handleInputBlur = () => {
+        setIsTyping(false);
+        if (query.trim() === '') {
+            setSuggestions([]);
+        }
+    };
 
     return (
         <View style={{ flex: 1 }}>
+            <Autocomplete
+                data={suggestions}
+                defaultValue={query}
+                onChangeText={handleInputChange}
+                onBlur={handleInputBlur}
+                flatListProps={{
+                    keyExtractor: (_, idx) => idx.toString(),
+                    renderItem: ({ item }) => (
+                        <TouchableOpacity onPress={() => setQuery(item)}>
+                            <Text>{item}</Text>
+                        </TouchableOpacity>
+                    ),
+                }}
+            />
             <MapView showsUserLocation={true} style={styles.map} region={location} />
             <View style={styles.absoluteBox}>
                 <TouchableOpacity style={styles.button} onPress={() => navigation.goBack()}>
