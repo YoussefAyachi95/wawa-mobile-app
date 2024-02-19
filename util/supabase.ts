2b85e3fa652af9de5328064f547c4a329669ad6e
Youssef <youssef1995elam@gmail.com>
2024-02-19 15:04:31 +0100

    Feature: Add supabase client

---
+++ util/supabase.ts
@@ -0,0 +1,8 @@
+import 'react-native-url-polyfill/auto'
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.EXPO_PUBLIC_API_URL; 
+const supabaseKey = process.env.EXPO_PUBLIC_API_KEY; 
+const supabase = createClient(supabaseUrl as string, supabaseKey as string);
+
+export default supabase;
